8c134a640bf21e0b80238da78a3113d2c78a3aa4
sayranovv <sayranoveldar@gmail.com>
2025-09-19 12:24:25 +0500

    Create Login & Signup requests

---
+++ server/src/main/java/com/example/demo/payload/request/SignupRequest.java
@@ -0,0 +1,30 @@
+package com.example.demo.payload.request;
+
+import com.example.demo.annotations.PasswordMatches;
+import com.example.demo.annotations.ValidEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@PasswordMatches
+public class SignupRequest {
+
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    @ValidEmail
+    private String email;
+    @NotEmpty(message = "Please enter your name")
+    private String firstname;
+    @NotEmpty(message = "Please enter your lastname")
+    private String lastname;
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+    private String confirmPassword;
+
+}
