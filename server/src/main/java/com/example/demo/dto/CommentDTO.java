c4b8bfd397d594575054c572be8c69952094f4d4
sayranovv <sayranoveldar@gmail.com>
2025-09-27 11:01:26 +0500

    Create CommentDTO

---
+++ server/src/main/java/com/example/demo/dto/CommentDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class CommentDTO {
+
+    private Long id;
+    @NotEmpty
+    private String message;
+    private String username;
+}
